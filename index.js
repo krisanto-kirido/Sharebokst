f1d64b301d8d3a52c788842de65cdd39cd3b5460
krisanto352 <krisantokirido@gmail.com>
2025-04-30 17:14:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const allowedOrigin = ["https://autoshareby-kristbella.vercel.app", "https://autoshareby-kristbella.vercel.app"];
+const allowedOrigin = ["https://autoshareby-kristbella.vercel.app", "https://vercel.com/krisanto352s-projects/autoshare.by-kris-bella-ylla-ybrahim-kirby"];
 
 app.use(cors());
 app.use(express.json());
